ea22a93435654fee891f2c88754e798fb5eb24ad
DESKTOP-G7EBHT0\yanna <yannay.jaffe@gmail.com>
2018-06-05 00:55:16 +0300

    created the entire project, need to be tested on nova

---
+++ Ex3/whole_project/Game.h
@@ -0,0 +1,114 @@
+//
+// Created by yanna on 05/07/18.
+//
+
+#ifndef EX2_GAME_H
+#define EX2_GAME_H
+// this class manages the game between the players
+#include <string>
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include "PlayerAlgorithm.h"
+#include "Board.h"
+#include "BoardImpl.h"
+#include "PieceCounter.h"
+#include "FightInfo.h"
+#include "FightInfoImpl.h"
+#include "PiecePosition.h"
+#include "PiecePositionImpl.h"
+
+
+bool operator>(const PiecePosition &p1, const PiecePosition &p2);
+
+bool operator<(const PiecePosition &p1, const PiecePosition &p2);
+
+bool operator==(const PiecePosition &p1, const PiecePosition &p2);
+
+class Game
+{
+public:
+    Game(std::unique_ptr<PlayerAlgorithm> player1,
+         std::unique_ptr<PlayerAlgorithm> player2); // provide the inputs from the command line in order to determine the game mode
+
+    int runGame(); // returns the winner id. 0 for tie
+
+private:
+
+
+
+    int
+    gameInit(); //this method performs all initialization and returns winner id or 0 if tie. -1 if no winner after init
+
+    bool checkInitialLegalPieces(
+            const std::vector<std::unique_ptr<PiecePosition>> &playerPieces) const; //this method receives a vector of pieces, and determines if its legal. use only in initialization
+
+    bool pointInBoard(const Point &p) const; // this method verifies that a point is within the board limits
+
+    void initBoard(); // this method initializes the game board, conducts all fights, and updates the FightInfo vector
+
+    void initPlayerOnBoard(
+            int playerId); // this method initializes only the player whos id is playerId on the board (and conducts fights...)
+
+    int getPieceCount(int playerId,
+                      char pieceType); //counts the remaining pieces of specified type of the specified player. if asked for jokers, count jokers. else count also joker current type
+
+    std::unique_ptr<PiecePosition> &
+    getPlayerPiece(int playerId, const Point &point); // returns the wanted player piece, nullptr if not found
+    std::unique_ptr<PiecePosition> nullPiecePosition = nullptr; // in order to return a nullptr and not throw exception from the above method
+    std::unique_ptr<FightInfo> getFightInfo(const PiecePosition &player1Piece,
+                                            const PiecePosition &player2Piece); // conducts the fight between the pieces and generates the FightInfo
+
+    bool isLegalMove(const Move &move, int playerId); // checks if a move is legal
+
+    bool isMovable(const PiecePosition &piece);//returns true if the piece can be moved
+
+    bool isSingleStep(const Move &move); // this method returns true only when the move is one step up/down/left/right.
+
+    std::unique_ptr<FightInfo>
+    playMove(const Move &move,
+             int playerId); //plays the move and updates player's piece vectors accordingly. returns the fight info
+
+    bool doFullMove(int playerId); // this method performs an entire move. return true if a winner is not declared
+
+    bool doJokerChange(int playerId,
+                       const JokerChange &jokerChange); // this method performs a joker change by the player. return true for success, false for illegal joker change
+
+    int
+    winConditionFlags();// returns 1 if player 1 has flags and player 2 doesnt. returns 2 if player 2 has flags and player 1 doesnt. returns 0 if both players dont have flags. return -1 if both have flags
+
+
+
+
+    const int BOTH = 0;
+    const int NONE = 0;
+    const int PLAYER1 = 1;
+    const int PLAYER2 = 2;
+
+    const int totalXVals = 10; //number of columns in the board (total x values)
+    const int totalYVals = 10; // number of rows in the board (total y values)
+    const int totalRocks = 2; // total Rocks
+    const int totalPapers = 5; // total Papers
+    const int totalScissors = 1; // total Scissors
+    const int totalBombs = 2; // total Bombs
+    const int totalJokers = 2; // total Jokers
+    const int totalFlags = 1; // total Flags
+    const int maxMoves = 100; //maximum moves until a tie is declared
+
+    int totalMoves;
+    int player1Moves;
+    int player2Moves;
+
+
+    std::unique_ptr<PlayerAlgorithm> player1;
+    std::unique_ptr<PlayerAlgorithm> player2;
+    std::vector<std::unique_ptr<PiecePosition>> player1Pieces; // an element could be nullptr
+    std::vector<std::unique_ptr<PiecePosition>> player2Pieces; //an element could be nullptr
+    BoardImpl board;
+    std::vector<std::unique_ptr<FightInfo>> fights;
+
+};
+
+
+#endif //EX2_GAME_H
